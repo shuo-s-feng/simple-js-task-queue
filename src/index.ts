fe464c51bbe5be78bbbf8a5eb6c05e3200ceb546
Sup-Friday <44722646+Sup-Friday@users.noreply.github.com>
2022-12-08 22:12:01 +0800

    Add prettier config

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export type TaskId = string | number;
 /**
  * The status of the task
  */
-export type TaskStatus = "idle" | "running" | "success" | "error";
+export type TaskStatus = 'idle' | 'running' | 'success' | 'error';
 /**
  * The unique ID of the queue
  */
@@ -22,37 +22,37 @@ type Reject = (error: Error) => void;
  * Task status changes handler
  */
 export type TaskStatusUpdateHandler<ReturnType> = (
-  status: TaskStatus,
-  task: Task<ReturnType>
+	status: TaskStatus,
+	task: Task<ReturnType>,
 ) => void;
 
 /**
  * The task object
  */
 export interface Task<ReturnType = any> {
-  taskId: TaskId;
-  status: TaskStatus;
-  callback: () => ReturnType | Promise<ReturnType>;
-  onStatusUpdate?: TaskStatusUpdateHandler<ReturnType>;
-  createdAt: number;
-  queueId?: QueueId;
-  result?: ReturnType;
-  error?: Error;
-  runAt?: number;
-  finishedAt?: number;
+	taskId: TaskId;
+	status: TaskStatus;
+	callback: () => ReturnType | Promise<ReturnType>;
+	onStatusUpdate?: TaskStatusUpdateHandler<ReturnType>;
+	createdAt: number;
+	queueId?: QueueId;
+	result?: ReturnType;
+	error?: Error;
+	runAt?: number;
+	finishedAt?: number;
 }
 
 interface WaitedTask<ReturnType = any> extends Task<ReturnType> {
-  resolve: Resolve<ReturnType>;
-  reject: Reject;
-  promise: Promise<ReturnType>;
+	resolve: Resolve<ReturnType>;
+	reject: Reject;
+	promise: Promise<ReturnType>;
 }
 
 interface PromiseQueue {
-  queueId: QueueId;
-  promise: Promise<any>;
-  length: number;
-  taskIds: Array<TaskId>;
+	queueId: QueueId;
+	promise: Promise<any>;
+	length: number;
+	taskIds: Array<TaskId>;
 }
 
 const RANDOM_BASE = 100000000;
@@ -60,342 +60,342 @@ const RANDOM_BASE = 100000000;
 const getRandomTaskId = () => Math.floor(Math.random() * RANDOM_BASE);
 
 const isTaskId = (obj: any): obj is TaskId => {
-  if (typeof obj === "string" || typeof obj === "number") {
-    return true;
-  }
-  return false;
+	if (typeof obj === 'string' || typeof obj === 'number') {
+		return true;
+	}
+	return false;
 };
 
 const isWaitedTask = (obj: any): obj is WaitedTask => {
-  if (obj.resolve && obj.promise) {
-    return true;
-  }
-  return false;
+	if (obj.resolve && obj.promise) {
+		return true;
+	}
+	return false;
 };
 
 export interface TaskQueueProps {
-  /**
-   * Maximum number of the concurrent running tasks
-   */
-  concurrency: number;
-  /**
-   * If true, the error of executing the tasks will be returned (resolved to the returning promise)
-   */
-  returnError?: boolean;
-  /**
-   * If true, getTask() and getAllTasks() will be available to retrieve task details
-   */
-  memorizeTasks?: boolean;
+	/**
+	 * Maximum number of the concurrent running tasks
+	 */
+	concurrency: number;
+	/**
+	 * If true, the error of executing the tasks will be returned (resolved to the returning promise)
+	 */
+	returnError?: boolean;
+	/**
+	 * If true, getTask() and getAllTasks() will be available to retrieve task details
+	 */
+	memorizeTasks?: boolean;
 }
 
 export class TaskQueue {
-  /** @internal */
-  private returnError: boolean;
-  /** @internal */
-  private memorizeTasks: boolean;
-  /** @internal */
-  private promiseQueues: Array<PromiseQueue> = [];
-  /** @internal */
-  private promiseQueueCapacity: number = 1;
-  /** @internal */
-  private taskLookup: Record<TaskId, Task> = {};
-  /** @internal */
-  private tasksWaitingQueue: Array<WaitedTask> = [];
-  /** @internal */
-  private taskIdRegistry: Set<TaskId> = new Set();
+	/** @internal */
+	private returnError: boolean;
+	/** @internal */
+	private memorizeTasks: boolean;
+	/** @internal */
+	private promiseQueues: Array<PromiseQueue> = [];
+	/** @internal */
+	private promiseQueueCapacity: number = 1;
+	/** @internal */
+	private taskLookup: Record<TaskId, Task> = {};
+	/** @internal */
+	private tasksWaitingQueue: Array<WaitedTask> = [];
+	/** @internal */
+	private taskIdRegistry: Set<TaskId> = new Set();
 
-  constructor({ concurrency, returnError, memorizeTasks }: TaskQueueProps) {
-    if (concurrency < 1) {
-      throw Error(`Invalid concurrency ${concurrency}`);
-    }
+	constructor({ concurrency, returnError, memorizeTasks }: TaskQueueProps) {
+		if (concurrency < 1) {
+			throw Error(`Invalid concurrency ${concurrency}`);
+		}
 
-    this.returnError = returnError ?? false;
-    this.memorizeTasks = memorizeTasks ?? false;
+		this.returnError = returnError ?? false;
+		this.memorizeTasks = memorizeTasks ?? false;
 
-    this.promiseQueues = new Array(concurrency).fill(null).map((_, index) => ({
-      queueId: index,
-      promise: Promise.resolve(),
-      length: 0,
-      taskIds: [],
-    }));
-  }
+		this.promiseQueues = new Array(concurrency).fill(null).map((_, index) => ({
+			queueId: index,
+			promise: Promise.resolve(),
+			length: 0,
+			taskIds: [],
+		}));
+	}
 
-  /** @internal */
-  private _updateTaskStatus(task: Task, status: TaskStatus) {
-    task.status = status;
-    task.onStatusUpdate?.(status, task);
-  }
+	/** @internal */
+	private _updateTaskStatus(task: Task, status: TaskStatus) {
+		task.status = status;
+		task.onStatusUpdate?.(status, task);
+	}
 
-  /** @internal */
-  private _getAvailablePromiseQueue() {
-    let bestQueueIndex = -1;
-    let bestLength = Infinity;
-    this.promiseQueues.forEach((queue, index) => {
-      if (queue.length < bestLength) {
-        bestLength = queue.length;
-        bestQueueIndex = index;
-      }
-    });
+	/** @internal */
+	private _getAvailablePromiseQueue() {
+		let bestQueueIndex = -1;
+		let bestLength = Infinity;
+		this.promiseQueues.forEach((queue, index) => {
+			if (queue.length < bestLength) {
+				bestLength = queue.length;
+				bestQueueIndex = index;
+			}
+		});
 
-    return this.promiseQueues[bestQueueIndex];
-  }
+		return this.promiseQueues[bestQueueIndex];
+	}
 
-  /** @internal */
-  private async _run<ReturnType>(
-    task: Task<ReturnType>,
-    resolve: Resolve<ReturnType>,
-    reject: Reject
-  ) {
-    try {
-      if (!this.taskIdRegistry.has(task.taskId) && task.status === "idle") {
-        this.taskIdRegistry.add(task.taskId);
+	/** @internal */
+	private async _run<ReturnType>(
+		task: Task<ReturnType>,
+		resolve: Resolve<ReturnType>,
+		reject: Reject,
+	) {
+		try {
+			if (!this.taskIdRegistry.has(task.taskId) && task.status === 'idle') {
+				this.taskIdRegistry.add(task.taskId);
 
-        task.runAt = new Date().getTime();
-        this._updateTaskStatus(task, "running");
+				task.runAt = new Date().getTime();
+				this._updateTaskStatus(task, 'running');
 
-        const result = await task.callback();
-        resolve(result);
+				const result = await task.callback();
+				resolve(result);
 
-        task.result = result;
-        this._updateTaskStatus(task, "success");
-        task.finishedAt = new Date().getTime();
-      } else {
-        throw Error(`Task ${task.taskId} is already triggered`);
-      }
-    } catch (error: any) {
-      task.error = error;
-      this._updateTaskStatus(task, "error");
+				task.result = result;
+				this._updateTaskStatus(task, 'success');
+				task.finishedAt = new Date().getTime();
+			} else {
+				throw Error(`Task ${task.taskId} is already triggered`);
+			}
+		} catch (error: any) {
+			task.error = error;
+			this._updateTaskStatus(task, 'error');
 
-      if (this.returnError) {
-        resolve(error);
-      } else {
-        reject(error);
-      }
-    }
-  }
+			if (this.returnError) {
+				resolve(error);
+			} else {
+				reject(error);
+			}
+		}
+	}
 
-  /** @internal */
-  private _addTask<ReturnType>(
-    task: Task<ReturnType> | WaitedTask<ReturnType>
-  ) {
-    if (this.memorizeTasks) {
-      this.taskLookup[task.taskId] = task;
-    }
+	/** @internal */
+	private _addTask<ReturnType>(
+		task: Task<ReturnType> | WaitedTask<ReturnType>,
+	) {
+		if (this.memorizeTasks) {
+			this.taskLookup[task.taskId] = task;
+		}
 
-    const _isWaitedTask = isWaitedTask(task);
+		const _isWaitedTask = isWaitedTask(task);
 
-    // The waited task already has the resolve function
-    let _resolve: Resolve<ReturnType>;
-    let _reject: Reject;
-    if (_isWaitedTask) {
-      _resolve = task.resolve;
-      _reject = task.reject;
-    }
+		// The waited task already has the resolve function
+		let _resolve: Resolve<ReturnType>;
+		let _reject: Reject;
+		if (_isWaitedTask) {
+			_resolve = task.resolve;
+			_reject = task.reject;
+		}
 
-    // The waited task already has the promise
-    const _promise: Promise<ReturnType> = _isWaitedTask
-      ? task.promise
-      : new Promise((resolve, reject) => {
-          _resolve = resolve;
-          _reject = reject;
-        });
+		// The waited task already has the promise
+		const _promise: Promise<ReturnType> = _isWaitedTask
+			? task.promise
+			: new Promise((resolve, reject) => {
+					_resolve = resolve;
+					_reject = reject;
+			  });
 
-    const promiseQueue = this._getAvailablePromiseQueue();
+		const promiseQueue = this._getAvailablePromiseQueue();
 
-    // If the promise queue reaches to its capacity, append the task to the waiting queue
-    if (promiseQueue.length >= this.promiseQueueCapacity) {
-      Promise.resolve().then(() => {
-        this.tasksWaitingQueue.push({
-          ...task,
-          resolve: _resolve,
-          reject: _reject,
-          promise: _promise,
-        });
-      });
-    }
+		// If the promise queue reaches to its capacity, append the task to the waiting queue
+		if (promiseQueue.length >= this.promiseQueueCapacity) {
+			Promise.resolve().then(() => {
+				this.tasksWaitingQueue.push({
+					...task,
+					resolve: _resolve,
+					reject: _reject,
+					promise: _promise,
+				});
+			});
+		}
 
-    // If the promise queue does not reach to its capacity, append the task to the promise queue
-    else {
-      promiseQueue.length += 1;
-      promiseQueue.taskIds.push(task.taskId);
-      task.queueId = promiseQueue.queueId;
+		// If the promise queue does not reach to its capacity, append the task to the promise queue
+		else {
+			promiseQueue.length += 1;
+			promiseQueue.taskIds.push(task.taskId);
+			task.queueId = promiseQueue.queueId;
 
-      const resolveTask = (
-        result: ReturnType | Error,
-        resolve: Resolve<ReturnType> | Reject
-      ) => {
-        promiseQueue.length -= 1;
-        promiseQueue.taskIds.shift();
+			const resolveTask = (
+				result: ReturnType | Error,
+				resolve: Resolve<ReturnType> | Reject,
+			) => {
+				promiseQueue.length -= 1;
+				promiseQueue.taskIds.shift();
 
-        resolve(result as any);
+				resolve(result as any);
 
-        // Get and re-add the first task from the waiting queue after the previous "Promise.resolve().then()" finishes appending tasks to the waiting queue
-        Promise.resolve().then(() => {
-          const nextTask = this.tasksWaitingQueue.shift();
+				// Get and re-add the first task from the waiting queue after the previous "Promise.resolve().then()" finishes appending tasks to the waiting queue
+				Promise.resolve().then(() => {
+					const nextTask = this.tasksWaitingQueue.shift();
 
-          if (nextTask) {
-            this._addTask(nextTask);
-          }
-        });
-      };
+					if (nextTask) {
+						this._addTask(nextTask);
+					}
+				});
+			};
 
-      const runTask = () =>
-        this._run(
-          task,
-          (result) => {
-            resolveTask(result, _resolve);
-          },
-          (error) => {
-            resolveTask(error, _reject);
-          }
-        );
+			const runTask = () =>
+				this._run(
+					task,
+					(result) => {
+						resolveTask(result, _resolve);
+					},
+					(error) => {
+						resolveTask(error, _reject);
+					},
+				);
 
-      // Keep running tasks no matter the previous task is resolved or rejected
-      promiseQueue.promise.then(runTask).catch(runTask);
+			// Keep running tasks no matter the previous task is resolved or rejected
+			promiseQueue.promise.then(runTask).catch(runTask);
 
-      // Append the promise to the promise queue to serialize async task executions
-      promiseQueue.promise = _promise;
-    }
+			// Append the promise to the promise queue to serialize async task executions
+			promiseQueue.promise = _promise;
+		}
 
-    // Return the result from the original callback function of the task
-    return _promise;
-  }
+		// Return the result from the original callback function of the task
+		return _promise;
+	}
 
-  /**
-   * Add a task with callback function to the queue
-   * @param callback The callback function of the task
-   */
-  addTask<ReturnType>(
-    callback: () => ReturnType | Promise<ReturnType>
-  ): Promise<ReturnType>;
+	/**
+	 * Add a task with callback function to the queue
+	 * @param callback The callback function of the task
+	 */
+	addTask<ReturnType>(
+		callback: () => ReturnType | Promise<ReturnType>,
+	): Promise<ReturnType>;
 
-  /**
-   * Add a task with callback function to the queue
-   * @param callback The callback function of the task
-   * @param onStatusUpdate The callback function to subscribe task status changes
-   */
-  addTask<ReturnType>(
-    callback: () => ReturnType | Promise<ReturnType>,
-    onStatusUpdate?: TaskStatusUpdateHandler<ReturnType>
-  ): Promise<ReturnType>;
+	/**
+	 * Add a task with callback function to the queue
+	 * @param callback The callback function of the task
+	 * @param onStatusUpdate The callback function to subscribe task status changes
+	 */
+	addTask<ReturnType>(
+		callback: () => ReturnType | Promise<ReturnType>,
+		onStatusUpdate?: TaskStatusUpdateHandler<ReturnType>,
+	): Promise<ReturnType>;
 
-  /**
-   * Add a task with callback function to the queue
-   * @param callback The callback function of the task
-   * @param taskId The ID of the task
-   * @param onStatusUpdate The callback function to subscribe task status changes
-   */
-  addTask<ReturnType>(
-    callback: () => ReturnType | Promise<ReturnType>,
-    taskId?: TaskId | undefined,
-    onStatusUpdate?: TaskStatusUpdateHandler<ReturnType>
-  ): Promise<ReturnType>;
+	/**
+	 * Add a task with callback function to the queue
+	 * @param callback The callback function of the task
+	 * @param taskId The ID of the task
+	 * @param onStatusUpdate The callback function to subscribe task status changes
+	 */
+	addTask<ReturnType>(
+		callback: () => ReturnType | Promise<ReturnType>,
+		taskId?: TaskId | undefined,
+		onStatusUpdate?: TaskStatusUpdateHandler<ReturnType>,
+	): Promise<ReturnType>;
 
-  /** @internal */
-  async addTask<ReturnType>(
-    callback: () => ReturnType | Promise<ReturnType>,
-    taskIdOrOnStatusUpdate?: TaskId | TaskStatusUpdateHandler<ReturnType>,
-    onStatusUpdate?: TaskStatusUpdateHandler<ReturnType>
-  ): Promise<ReturnType> {
-    if (
-      isTaskId(taskIdOrOnStatusUpdate) ||
-      taskIdOrOnStatusUpdate === undefined
-    ) {
-      return this._addTask({
-        taskId: taskIdOrOnStatusUpdate ?? getRandomTaskId(),
-        callback,
-        createdAt: new Date().getTime(),
-        status: "idle",
-        onStatusUpdate,
-      });
-    } else {
-      return this._addTask({
-        taskId: getRandomTaskId(),
-        callback,
-        createdAt: new Date().getTime(),
-        status: "idle",
-        onStatusUpdate: taskIdOrOnStatusUpdate,
-      });
-    }
-  }
+	/** @internal */
+	async addTask<ReturnType>(
+		callback: () => ReturnType | Promise<ReturnType>,
+		taskIdOrOnStatusUpdate?: TaskId | TaskStatusUpdateHandler<ReturnType>,
+		onStatusUpdate?: TaskStatusUpdateHandler<ReturnType>,
+	): Promise<ReturnType> {
+		if (
+			isTaskId(taskIdOrOnStatusUpdate) ||
+			taskIdOrOnStatusUpdate === undefined
+		) {
+			return this._addTask({
+				taskId: taskIdOrOnStatusUpdate ?? getRandomTaskId(),
+				callback,
+				createdAt: new Date().getTime(),
+				status: 'idle',
+				onStatusUpdate,
+			});
+		} else {
+			return this._addTask({
+				taskId: getRandomTaskId(),
+				callback,
+				createdAt: new Date().getTime(),
+				status: 'idle',
+				onStatusUpdate: taskIdOrOnStatusUpdate,
+			});
+		}
+	}
 
-  /**
-   * Add tasks with callback functions to the queue
-   * @param tasks The array of tasks with callback functions, IDs, and task status changes subscriber
-   */
-  async addTasks(
-    tasks: Array<{
-      callback: () => unknown;
-      taskId?: TaskId;
-      onStatusUpdate?: TaskStatusUpdateHandler<unknown>;
-    }>
-  ) {
-    return Promise.all(
-      tasks.map((task) =>
-        this.addTask(
-          task.callback,
-          task.taskId ?? getRandomTaskId(),
-          task.onStatusUpdate
-        )
-      )
-    );
-  }
+	/**
+	 * Add tasks with callback functions to the queue
+	 * @param tasks The array of tasks with callback functions, IDs, and task status changes subscriber
+	 */
+	async addTasks(
+		tasks: Array<{
+			callback: () => unknown;
+			taskId?: TaskId;
+			onStatusUpdate?: TaskStatusUpdateHandler<unknown>;
+		}>,
+	) {
+		return Promise.all(
+			tasks.map((task) =>
+				this.addTask(
+					task.callback,
+					task.taskId ?? getRandomTaskId(),
+					task.onStatusUpdate,
+				),
+			),
+		);
+	}
 
-  /** @internal */
-  private _assertMemorizingTasksEnabled() {
-    if (!this.memorizeTasks) {
-      throw Error(
-        "Memorizing task details is not enabled. Please update the memorizeTasks configuration when initializing the queue instance"
-      );
-    }
-  }
+	/** @internal */
+	private _assertMemorizingTasksEnabled() {
+		if (!this.memorizeTasks) {
+			throw Error(
+				'Memorizing task details is not enabled. Please update the memorizeTasks configuration when initializing the queue instance',
+			);
+		}
+	}
 
-  /**
-   * Get the task details with task ID
-   * @param taskId The ID of the task
-   */
-  getTask(taskId: TaskId) {
-    this._assertMemorizingTasksEnabled();
+	/**
+	 * Get the task details with task ID
+	 * @param taskId The ID of the task
+	 */
+	getTask(taskId: TaskId) {
+		this._assertMemorizingTasksEnabled();
 
-    return this.taskLookup[taskId];
-  }
+		return this.taskLookup[taskId];
+	}
 
-  /**
-   * Get all task details with matching status
-   * @param status The matched status or array of statuses
-   */
-  getAllTasks(status?: TaskStatus | Array<TaskStatus>) {
-    this._assertMemorizingTasksEnabled();
+	/**
+	 * Get all task details with matching status
+	 * @param status The matched status or array of statuses
+	 */
+	getAllTasks(status?: TaskStatus | Array<TaskStatus>) {
+		this._assertMemorizingTasksEnabled();
 
-    const allTasks = Object.values(this.taskLookup);
+		const allTasks = Object.values(this.taskLookup);
 
-    if (Array.isArray(status)) {
-      return allTasks.filter((task) => status.includes(task.status));
-    } else {
-      return status
-        ? allTasks.filter((task) => task.status === status)
-        : allTasks;
-    }
-  }
+		if (Array.isArray(status)) {
+			return allTasks.filter((task) => status.includes(task.status));
+		} else {
+			return status
+				? allTasks.filter((task) => task.status === status)
+				: allTasks;
+		}
+	}
 
-  /**
-   * Clear the task details with task ID
-   * @param taskId The ID of the task
-   */
-  clearTask(taskId: TaskId) {
-    this._assertMemorizingTasksEnabled();
+	/**
+	 * Clear the task details with task ID
+	 * @param taskId The ID of the task
+	 */
+	clearTask(taskId: TaskId) {
+		this._assertMemorizingTasksEnabled();
 
-    delete this.taskLookup[taskId];
-  }
+		delete this.taskLookup[taskId];
+	}
 
-  /**
-   * Clear all task details
-   */
-  clearAllTasks() {
-    this._assertMemorizingTasksEnabled();
+	/**
+	 * Clear all task details
+	 */
+	clearAllTasks() {
+		this._assertMemorizingTasksEnabled();
 
-    this.taskLookup = {};
-  }
+		this.taskLookup = {};
+	}
 }
 
 export default TaskQueue;
